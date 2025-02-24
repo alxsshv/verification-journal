4e73c1c47953ee124d7a1b83191fa8388fa08781
aleksei_shvariov <alxsshv@gmail.com>
2025-02-24 13:33:19 +0300

    #18 Addition: - has been updated checkstyle version; - default page number set 0; - added catching constraintViolationException; - fieldsa in JournalDto and ProtocolDto classes @notNull annotation has been changed to @notEmpty;

--- src/main/java/com/alxsshv/config/AppConstants.java
+++ src/main/java/com/alxsshv/config/AppConstants.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 
 @Getter
 public class AppConstants {
-    public static final String DEFAULT_PAGE_NUMBER = "1";
+    public static final String DEFAULT_PAGE_NUMBER = "0";
     public static final String DEFAULT_PAGE_SIZE = "10";
     public static final String DEFAULT_PAGE_SORT_BY = "id";
     public static final String DEFAULT_PAGE_SORT_DIR = "ASC";
